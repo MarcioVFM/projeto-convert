0365e47376d191cedf01103d53dfa07a4971af0f
Marcio Vinicius <marciov859@gmail.com>
2025-01-02 14:08:46 -0400

    colocando a contações das moedas, criando o onsubimit do formulario para pegar as informacoes

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-const amount = document.getElementById('amount')
+const USD = 6.1
+const EUR = 6.3
+const GPB = 7.6
+
+//obtendo os elementos do fomulario
+const form = document.querySelector('form')
+const amount = document.getElementById('amount')//valor no input
+const currency = document.getElementById('currency')
+
 
 //manipulação da variavel amout para que ela so receba valores numericos
 amount.addEventListener('input', () => {
     hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, '')
 })
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch (currency.value) {
+        case 'USD':
+            convertCurrency(amount.value, USD, 'US$')
+            break
+        case 'EUR':
+            convertCurrency(amount.value, EUR, '€')
+            break
+        case 'GPB':
+            convertCurrency(amount.value, GPB, '£')
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
+}
